7087d055a88735f438bc8f130b5779a5f631ed0f
Martha Plantz <maplantz89@gmail.com>
2019-08-20 10:50:10 -0700

    deploying

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://frozen-ocean-21928.herokuapp.com/',
+  API_ENDPOINT: 'https://guarded-ocean-97358.herokuapp.com',
   TOKEN_KEY: process.env.REACT_APP_API_KEY,
 }
