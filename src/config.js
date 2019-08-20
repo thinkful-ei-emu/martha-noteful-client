115a88735c459e08e738ff0d247bea3ad259435f
Martha Plantz <maplantz89@gmail.com>
2019-08-20 11:12:42 -0700

    request to api not working

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://guarded-ocean-97358.herokuapp.com',
+  API_ENDPOINT: 'https://polar-depths-46444.herokuapp.com',
   TOKEN_KEY: process.env.REACT_APP_API_KEY,
 }
